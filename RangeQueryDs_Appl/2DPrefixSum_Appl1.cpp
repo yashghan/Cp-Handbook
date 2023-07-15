870bcf9eb943fc2a060eeb8c0e78a63825593071
yashghan <64274477+yashghan@users.noreply.github.com>
2023-07-15 12:07:23 +0530

    Create 2DPrefixSum_Appl1.cpp

---
+++ RangeQueryDs_Appl/2DPrefixSum_Appl1.cpp
@@ -0,0 +1,39 @@
+
+class Solution {
+public:
+    long long countQuadruplets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> greaterThan(n, vector<int> (n, 0)), lesserThan(n, vector<int> (n, 0));
+
+        //let greaterThan[i][j] represent number of elements in the range [i..j] which are greater than nums[i]
+
+        //let lesserThan[i][j] represent number of elements in the range [i..j] which are lesser than nums[j]
+        for(int i = 0; i < n; i++){
+            for(int j = i+1, greater = 0; j < n; j++){
+                greater += (nums[j] > nums[i]);
+                greaterThan[i][j] = greater;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = i - 1, smaller = 0; j >= 0; j--){
+                smaller += (nums[j] < nums[i]);
+                lesserThan[j][i] = smaller;
+            }
+        }
+
+        long long res = 0;
+        for(int j = 0; j < n-2; j++){
+            for(int k = j + 1; k < n - 1; k++){
+                if(nums[j] > nums[k]){
+                    int poss_i = (lesserThan[0][k] - lesserThan[j][k]);
+                    int poss_l = (greaterThan[j][n-1] - greaterThan[j][k]);
+                    res += 1LL * poss_i * poss_l;
+                }
+            }
+        }
+
+        return res;
+
+    }
+};
